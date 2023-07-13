c7a2e2fec7833a75c4917f8087829aaa1729bcef
artsiomchekh <qbartem@gmail.com>
2023-07-13 15:18:11 +0300

    Combine return unsorted list

--- src/test/scala/patmat/HuffmanSuite.scala
+++ src/test/scala/patmat/HuffmanSuite.scala
@@ -1,13 +1,27 @@
 package patmat
 
 class HuffmanSuite extends munit.FunSuite:
+
   import Huffman.*
 
   trait TestTrees {
-    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
-    val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
+    val t1 = Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5)
+    val t2 = Fork(Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5), Leaf('d', 4), List('a', 'b', 'd'), 9)
+    val charsList = List('a', 'a', 'b', 'b', 'b', 'c')
+    val tree: CodeTree = t1
+    var listOfTrees: List[CodeTree] = List()
   }
 
+  test("def times") {
+    new TestTrees:
+      assertEquals(times(charsList), List(('a', 2), ('b', 3), ('c', 1)))
+  }
+
+  test("def singleton") {
+    new TestTrees:
+      listOfTrees = tree :: listOfTrees
+      assert(singleton(listOfTrees))
+  }
 
   test("weight of a larger tree (10pts)") {
     new TestTrees:
@@ -17,7 +31,7 @@ class HuffmanSuite extends munit.FunSuite:
 
   test("chars of a larger tree (10pts)") {
     new TestTrees:
-      assertEquals(chars(t2), List('a','b','d'))
+      assertEquals(chars(t2), List('a', 'b', 'd'))
   }
 
   test("string2chars hello world") {
@@ -26,13 +40,15 @@ class HuffmanSuite extends munit.FunSuite:
 
 
   test("make ordered leaf list for some frequency table (15pts)") {
-    assertEquals(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))), List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
+    assertEquals(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))), List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 3)))
   }
 
 
   test("combine of some leaf list (15pts)") {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
-    assertEquals(combine(leaflist), List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
+    val leaflist2 = List(Leaf('e', 3), Leaf('t', 2), Leaf('x', 4))
+    assertEquals(combine(leaflist), List(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4)))
+    assertEquals(combine(leaflist2), List(Leaf('x', 4), Fork(Leaf('e', 3), Leaf('t', 2), List('e', 't'), 5)))
   }
 
 
@@ -43,4 +59,5 @@ class HuffmanSuite extends munit.FunSuite:
 
 
   import scala.concurrent.duration.*
+
   override val munitTimeout = 10.seconds
